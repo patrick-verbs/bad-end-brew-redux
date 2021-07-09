b862511e88b3c476a0e7a5cc5b08b352cc7a1449
Patrick Lee <patricklee1138@live.com>
2021-07-09 13:34:19 -0700

    add testing seed data

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,6 +1,16 @@
 import kegListReducer from "../../reducers/keg-list-reducer";
 
 describe("kegListReducer", () => {
+  let action;
+  const kegData = {
+    name: 'The Green Dragon',
+    brand: 'Against the Grain',
+    price: '5.00',
+    alcoholContent: '12.5',
+    pintsLeft: 124,
+    id: 1
+  };
+
   test("Should return default state if there is no action type passed into the reducer", () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
   });
