e5d04fb3694c0e20a7732492b4b785084737efa5
Patrick Lee <patricklee1138@live.com>
2021-07-09 13:40:47 -0700

    RED: test adding kegs to list

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -14,4 +14,29 @@ describe("kegListReducer", () => {
   test("Should return default state if there is no action type passed into the reducer", () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
   });
+
+  test("Should successfully add new keg data to masterKegList", () => {
+    const { name, brand, price, alcoholContent, pintsLeft, id } = kegData;
+    action = {
+      type: 'ADD_KEG',
+      name: name,
+      brand: brand,
+      price: price,
+      alcoholContent: alcoholContent,
+      pintsLeft: pintsLeft,
+      id: id
+    };
+
+    expect(kegListReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        brand: brand,
+        price: price,
+        alcoholContent: alcoholContent,
+        pintsLeft: pintsLeft,
+        id: id
+      }
+    });
+  });
+
 });
