3fd8ef5c9787e12f01a5e45afd07f70120eb5395
Patrick Lee <patricklee1138@live.com>
2021-07-10 21:45:35 -0700

    RED: test deleting kegs from list

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -11,6 +11,25 @@ describe("kegListReducer", () => {
     id: 1
   };
 
+  const currentState = {
+    1: {
+      name: 'The Green Dragon',
+      brand: 'Against the Grain',
+      price: '5.00',
+      alcoholContent: '12.5',
+      pintsLeft: 124,
+      id: 1
+    },
+    2: {
+      name: 'The Hobbit: Smaug Stout',
+      brand: 'Fish Tale',
+      price: '5.00',
+      alcoholContent: '9.5',
+      pintsLeft: 124,
+      id: 1
+    }
+  }
+
   test("Should return default state if there is no action type passed into the reducer", () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
   });
@@ -35,6 +54,23 @@ describe("kegListReducer", () => {
         alcoholContent: alcoholContent,
         pintsLeft: pintsLeft,
         id: id
+      }
+    });
+  });
+
+  test("Should successfully delete a keg", () => {
+    action = {
+      type: 'DELETE_KEG',
+      id: 1
+    };
+    expect(kegListReducer(currentState, action)).toEqual({
+      2: {
+        name: 'The Hobbit: Smaug Stout',
+        brand: 'Fish Tale',
+        price: '5.00',
+        alcoholContent: '9.5',
+        pintsLeft: 124,
+        id: 1
       }
     });
   });
